0f9f4e8db6f9da4fe92102211fefc69b3d03e301
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-10 10:22:49 +0600

    add varivle

--- var/var.cpp
+++ var/var.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+ #include <string>
 
 int main(){
     int ok = 10;
-    double okk = 1001;
+    double okk = 100.1;
+    // string letter = "Hello world";
+
     std::cout << ok+okk;
     return 0;
 }
