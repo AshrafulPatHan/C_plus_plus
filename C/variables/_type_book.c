147e63340af9b2ad04c7479326fccdd177914154
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-04 21:24:45 +0600

    _type of the change value

---
+++ C/variables/_type_book.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    ch = 'X';
+    printf("The Value is %c\n", ch);
+
+    ch = 'Y';
+    printf("The Value is %c\n", ch);
+    
+    return 0;
+}
