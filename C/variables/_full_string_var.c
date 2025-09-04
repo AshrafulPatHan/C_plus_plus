4b599b925279d7ae488d89cd30e7b629da9761ba
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-04 21:35:19 +0600

    the string is add from of array

---
+++ C/variables/_full_string_var.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[] = "my name is ashraful";
+    char *name = "my name is pathan";
+    printf("The Value is: %s\n", str);
+    printf("The Value is: %s\n", name);
+    return 0;
+}
