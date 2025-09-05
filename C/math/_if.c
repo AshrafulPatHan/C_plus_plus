756207389fabe823521858b81defa1e358bc8c30
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-05 22:16:58 +0600

    i learn the fi else on c

--- C/math/_if.c
+++ C/math/_if.c
@@ -2,11 +2,13 @@
 
 int main()
 {
-    int n1 = 5,n2 = 5;
+    int n1 = 5,n2 = 7;
     if (n1 == n2)
     {
         printf("Number is equal");
     }
-    printf("Number is not equal");
+    else {
+        printf("Number is not equal");
+    }
     return 0;
 }
