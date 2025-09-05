7e43b700af57986831290ce97ca193e2326e45a2
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-05 22:42:59 +0600

    add for loop

---
+++ C/math/_for_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    for(n=1;n <= 100;n=n+1)
+    {
+        if ( n % 2 == 0 )
+        {
+            printf("%d\n",n);
+        }
+    }
+    printf("%d", 7 % 2);
+    return 0;
+}
