7ec74b26c2399972400d3d3eca44aad0b47fa168
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-11-04 18:13:08 +0600

    add

--- C/praktis/serijs.c
+++ C/praktis/serijs.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+// 2+4+6+8
+
 int main(){
     int a,s;
     s=0;
-    a=0;
+    a=2;
 
     // loop
-    while(a<=3){
-        s=s+a; 
-        a = a+1; 
+    while(a<=6){
+        s=s+a;
+        a = a+2; 
     }
     printf("%d \n",s);
     return 0;
