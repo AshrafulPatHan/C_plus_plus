1672a7eda3aba4ef41b848b72a731db32fab5321
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-12-12 18:55:57 +0600

    now i know about names

--- C/function/function.c
+++ C/function/function.c
@@ -9,9 +9,14 @@ void SumTowNum(int x,int y){
     printf("%d and %d sum is: %d\n", x, y, sum);
 }
 
+void aboutMe(char name[], int age) {
+  printf("Hello %s. You are %d years old.\n", name, age);
+}
+
 int main(){
-    myFunction();
-    SumTowNum(2,4);
+    myFunction(); 
+    SumTowNum(2,4); // sum function
+    aboutMe("ashraful",18);
     return 0;
 }
 
