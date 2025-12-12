5826aa3b6f161bf6822c2aedc36875576b6eaf55
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-12-12 18:46:53 +0600

    basic math on c function

--- C/function/function.c
+++ C/function/function.c
@@ -4,8 +4,14 @@ void myFunction(){
     printf("first function \n");
 }
 
+void SumTowNum(int x,int y){
+    int sum = x+y;
+    printf("%d and %d sum is: %d\n", x, y, sum);
+}
+
 int main(){
     myFunction();
+    SumTowNum(2,4);
     return 0;
 }
 
